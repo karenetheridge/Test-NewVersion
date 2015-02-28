40b22268e20d99415429fa42ddb9fc8de10d36e1
Karen Etheridge <ether@cpan.org>
2015-02-27 20:50:37 -0800

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.087, github profile.

---
+++ lib/Test/NewVersion.pm
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+package Test::NewVersion;
+# ABSTRACT: ...
+# KEYWORDS: ...
+# vim: set ts=8 sw=4 tw=78 et :
+
+our $VERSION = '0.001';
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Test::NewVersion;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=head2 C<foo>
+
+...
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Test-NewVersion>
+(or L<bug-Test-NewVersion@rt.cpan.org|mailto:bug-Test-NewVersion@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=for :list
+* L<foo>
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Test::NewVersion;
+
+fail('this test is TODO!');
+done_testing;
